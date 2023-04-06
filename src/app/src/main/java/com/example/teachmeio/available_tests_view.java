34c77db78331df7cad658985e38265bbb8aaf5e4
MassiGy <ghernaoutmassi@gmail.com>
2023-04-06 11:06:41 +0200

    +dev-massigy: renamed some files for better consistency

---
+++ src/app/src/main/java/com/example/teachmeio/available_tests_view.java
@@ -0,0 +1,14 @@
+package com.example.teachmeio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class available_tests_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_tests_view);
+    }
+}
--- src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
+++ src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class available_verbs_view extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.available_verbs_view);
     }
 }
