7d35648c37b188cb409e57595b23607e59554264
Antoine <antoine.montier@sfr.fr>
2023-04-06 12:08:10 +0200

    +dev-antoine: added Verbs class and Tabverb

---
+++ src/app/Verbs.java
@@ -0,0 +1,23 @@
+public class Verbs {
+    public String french;
+    public String english;
+    public String preterit;
+    public String past_p;
+
+    public boolean selected;
+
+    public Verbs(){
+        french = "";
+        english = "";
+        preterit = "";
+        past_p = "";
+        selected = true;
+    }
+    public Verbs(String f, String e, String pre, String p_p){
+        french = f;
+        english = e;
+        preterit = pre;
+        past_p = p_p;
+        selected = true;
+    }
+}
