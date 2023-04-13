35f3c4067ff8ed798729dd6368fcc1095c45deaa
MassiGy <ghernaoutmassi@gmail.com>
2023-04-13 12:59:58 +0200

    +dev-massigy: added the basic workflow of the application and set up the buttons linking

--- src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
+++ src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
@@ -2,13 +2,30 @@ package com.example.teachmeio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class available_verbs_view extends AppCompatActivity {
+    Button submit_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.available_verbs_view);
+
+        submit_btn = findViewById(R.id.submit_btn);
+
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code to be executed when button is clicked
+                startActivity(new Intent(available_verbs_view.this, available_tests_view.class));
+            }
+        });
     }
+
+
 }
