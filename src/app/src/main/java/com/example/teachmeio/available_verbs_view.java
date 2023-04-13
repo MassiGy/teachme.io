664453eab008d5d01eeb965735effcf753e7d967
Antoine <antoine.montier@sfr.fr>
2023-04-13 13:09:18 +0200

    +dev-antoine: tried to perform the selection of verbs but i have a problem of library

--- src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
+++ src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
@@ -1,24 +1,31 @@
 package com.example.teachmeio;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Switch;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class available_verbs_view extends AppCompatActivity {
 
-    RecyclerView rv;
+     ListView lv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        rv = findViewById(R.id.select_verbs);
+        lv = findViewById(R.id.verbs_selection);
         TabVerb tv = new TabVerb("./verbs.txt");
         List<String> verbs_list = tv.getList();
-        rv.setT
+
+        for(int i = 0  ; i < verbs_list.size() ; ++i){
+            Switch s = new Switch(available_verbs_view.this);
+            s.setText(verbs_list.get(i));
+            lv.addView(s);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.available_verbs_view);
     }
