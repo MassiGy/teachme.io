ab33919baea7473edaadab6350a4e81f75d7ec39
Antoine <antoine.montier@sfr.fr>
2023-04-14 11:43:08 +0200

    +dev-antoine: worked on the selection screen, need to allow the user to click on every verbs he wants

--- src/app/src/main/java/com/example/teachmeio/TabVerb.java
+++ src/app/src/main/java/com/example/teachmeio/TabVerb.java
@@ -9,8 +9,9 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 public class TabVerb {
-    private ArrayList<Verbs> arr;
+    public ArrayList<Verbs> arr;
 
+    public static int nb_verbs;
     public TabVerb(int size){
         arr = new ArrayList<Verbs>();
     }
@@ -21,6 +22,7 @@ public class TabVerb {
             String line1, line2, line3, line4;
             while ((line1 = reader.readLine()) != null && (line2 = reader.readLine()) != null && (line3 = reader.readLine()) != null && (line4 = reader.readLine()) != null) {
                 arr.add(new Verbs(line4, line1, line2, line3));
+                nb_verbs++;
             }
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
--- src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
+++ src/app/src/main/java/com/example/teachmeio/available_verbs_view.java
@@ -11,22 +11,18 @@ import java.util.List;
 
 public class available_verbs_view extends AppCompatActivity {
 
-     ListView lv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        lv = findViewById(R.id.verbs_selection);
-        TabVerb tv = new TabVerb("./verbs.txt");
-        List<String> verbs_list = tv.getList();
-
-        for(int i = 0  ; i < verbs_list.size() ; ++i){
-            Switch s = new Switch(available_verbs_view.this);
-            s.setText(verbs_list.get(i));
-            lv.addView(s);
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.available_verbs_view);
+
+        TabVerb tv = new TabVerb("./verbs.txt");
+        for(int i = 0 ; i  < TabVerb.nb_verbs ; ++i){
+            Switch s = (Switch)findViewById(R.id.switch0);
+            s.setText("blabla");//tv.arr.get(i).toString());
+            s.setChecked(tv.arr.get(i).selected);
+        }
     }
 }
